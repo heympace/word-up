a22ba945406ebddc3aa3a4403f7971e70b7afbc0
MPace <heympace2@gmail.com>
2019-01-28 19:19:00 -0600

    game over, disable and clear input

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -169,7 +169,8 @@ function render() {
     if (gameOver) {
         // TODO 9
         // disable the text box and clear its contents
-
+        $("#textbox").attr("disabled", true);
+        $("#textbox").val("");
     }
 }
 
